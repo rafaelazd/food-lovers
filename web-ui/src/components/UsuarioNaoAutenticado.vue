571f14a48fab83d750fb4a27edf99c7c5d3f1764
Rafaela Zanetti <rafazanettid@gmail.com>
2017-09-07 20:24:33 -0300

    Adicionada a página inicial para usuário não autenticado
    A página é acessada através do fragmento #usuario-nao-autenticado

---
+++ web-ui/src/components/UsuarioNaoAutenticado.vue
@@ -0,0 +1,44 @@
+<template>
+    <section id="cover">
+        <div id="cover-caption">
+            <div class="container">
+                <div class="text-center">
+                    <img src="/static/img/logo.png" class="img-fluid" alt="Logo">
+                    <div class="text-center" style="padding-top: 20px;">
+                        <router-link class="btn btn-outline-danger btn-lg" role="button" aria-pressed="true" to="/login">Entrar</router-link>
+                        <router-link class="btn btn-outline-danger btn-lg" role="button" aria-pressed="true" to="/cadastro">Cadastrar-se</router-link>
+                    </div>
+                </div>
+            </div>    
+        </div>
+    </section>
+</template>
+
+
+<script>
+</script>
+
+
+<style>
+    html, body {
+        height: 100%;
+        background: #222 url('/static/img/thanks1.png') center center no-repeat;
+        background-size: cover;
+        color: indianred;
+        height: 100%;
+
+    }
+    #cover {
+        background: #222 url('/static/img/thanks1.png') center center no-repeat;
+        background-size: cover;
+        color: indianred;
+        height: 100%;
+        text-align: center;
+        display: flex;
+        align-items: center;
+
+    }
+    #cover-caption {
+        width: 100%;
+    }
+</style>
--- web-ui/src/router/index.js
+++ web-ui/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import UsuarioNaoAutenticado from '@/components/UsuarioNaoAutenticado'
 
 Vue.use(Router)
 
@@ -10,6 +11,10 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: Hello
+    }, {
+      path: '/usuario-nao-autenticado',
+      name: 'UsuarioNaoAutenticado',
+      component: UsuarioNaoAutenticado
     }
   ]
 })
