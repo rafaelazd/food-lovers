cecf010b40e95f56f5dd300d2aeab660fd39e25f
Rafaela Zanetti <Rafaela Zanetti>
2017-09-07 20:04:46 -0300

    Adicionada a biblioteca bootstrap-vue

--- web-ui/src/main.js
+++ web-ui/src/main.js
@@ -2,7 +2,15 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+
 import router from './router'
+
+import BootstrapVue from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue);
 
 Vue.config.productionTip = false
 
