b14e68b62e9f80e36a6866e4e50102a68dbfc441
Rafaela Zanetti <rafazanettid@gmail.com>
2017-09-27 13:15:47 -0300

    Tela inicial
    Tela "Home" do sistema, primeiros passos na construção dos elementos (menu e perfis)

---
+++ web-ui/src/components/Inicio.vue
@@ -0,0 +1,41 @@
+<template>
+   <section id="inicio-cover">
+   <div class="container">
+   <b-navbar toggleable="md" type="dark" variant="info">
+    <b-nav-toggle target="nav_collapse"></b-nav-toggle>
+
+  <b-navbar-brand href="#"><b-img src="/static/img/foodlovers.png" fluid alt="Logo"/></b-navbar-brand>
+
+  <b-collapse is-nav id="nav_collapse">
+
+    <b-nav is-nav-bar>
+      <b-nav-item href="#">Perfil</b-nav-item>
+      <b-nav-item href="#">Preferências</b-nav-item>
+      <b-nav-item href="#">Combinações</b-nav-item>
+    </b-nav>
+
+  </b-collapse>
+   </b-navbar>
+   </div>
+  </section>
+</template>
+
+
+<script>
+
+
+</script>
+
+
+<style>
+    #inicio-cover {
+        height: 100vh;
+        width: 100vw;
+        background-size: cover;
+        display: flex;
+    }
+    
+    .container b-navbar {
+        left: 0;
+    }
+</style>
