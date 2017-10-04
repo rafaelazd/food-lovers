c2bffbd3b8990deb13c582e54fb0694d2820bd03
Rafaela Zanetti <rafazanettid@gmail.com>
2017-10-03 23:57:36 -0300

    Chat
    Adicionando o título, pequeno ícone de conversas e uma barra de pesquisa

--- web-ui/src/components/Chat.vue
+++ web-ui/src/components/Chat.vue
@@ -33,7 +33,19 @@
         <!-- Perfil Card -->
         <div class="row justify-content-center">
             <div class="app">
-               
+               <div class="head clearfix">
+                   <span class="messages-notification">
+                       <i class="fa fa-comments-o"></i>
+                       <span class="count">2</span>
+                   </span>
+                   <span class="title">Messenger</span>
+               </div>
+               <div class="search-bar">
+                   <b-input-group>
+                    <b-form-input v-model="search" type="search" class="ip-search" placeholder="Procurar..."></b-form-input>
+                    <button class="btn btn-secondary input-group-addon"><i class="fa fa-search"></i></button>
+                  </b-input-group>
+               </div>
             </div>
         </div>
         <!-- END Perfil Card -->
@@ -78,6 +90,94 @@
         box-shadow: 10px 20px 20px rgba(0,0,0,.5);
         position: relative;
         
+    }
+    
+    .app:before {
+        content: '';
+        position: absolute;
+        left: 0;
+        width: 100%;
+        height: 100px;
+        display: inline-block;
+        z-index: 0;
+    }
+    
+    .app:after {
+        content: '';
+        position: absolute;
+        left: 0;
+        width: 100%;
+        height: 100px;
+        display: inline-block;
+        bottom: 0;
+        z-index: 0;
+    }
+    
+    .app .head {
+        z-index: 1;
+        position: relative;
+        display: block;
+        width: 100%;
+        height: 50px;
+        line-height: 48px;
+        text-align: center;
+        text-transform: uppercase;
+        letter-spacing: 2px;
+        color: #fff;
+        background-color: #34495e;
+        box-shadow: inset 0 -15px 35px -5px rgba(0,0,0,0.3);
+        
+    }
+    
+    .head span.title {
+        position: absolute;
+        width: 100%;
+        display: inline-block;
+        left: 0;
+        font-size: 15px;
+        font-weight: 500;
+        margin-top: 2px;
+        color: #fff;
+    }
+    
+    .head span.messages-notification {
+        position: relative;
+        float: left;
+        font-size: 15px;
+        font-weight: normal;
+        padding-left: 20px;
+        color: white;
+        padding-top: 2px;
+        
+    }
+    
+    .head span.messages-notification span.count {
+        position: absolute;
+        font-size: 10px;
+        width: 17px;
+        height: 17px;
+        display: inline-block;
+        background-color: #e74c3c;
+        line-height: 17px;
+        border-radius: 100%;
+        margin-top: -40px;  
+    }
+    
+    .clearfix::after {
+        content: '';
+        clear: both;
+        display: table;
+    }
+    
+    .search-bar {
+        padding: 10px;
+        text-align: center;
+        z-index: 1;
+    }
+    
+    .search-bar .ip-search {
+        background-color: rgba(255,255,255,0.25);
+        color: #34495e;
     }
 
 </style>
