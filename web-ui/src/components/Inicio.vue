a29751c33454920ef63ffb8c80f5eeebd4d401e8
Rafaela Zanetti <rafazanettid@gmail.com>
2017-10-03 22:36:25 -0300

    Indentação e alterações
    Indentação do código e alterações no layout

--- web-ui/src/components/Inicio.vue
+++ web-ui/src/components/Inicio.vue
@@ -1,61 +1,62 @@
 <template>
    <section id="inicio-cover">
        <div class="container">
-   <!-- Navbar -->
+       <!-- Navbar -->
         <nav class="navbar navbar-toggleable-md navbar-dark navbar-fixed-top d-flex justify-content-between">
-              <b-nav-toggle target="nav_collapse"></b-nav-toggle>
-               <img src="/static/img/foodlovers.png" width="158px">
+                <b-nav-toggle target="nav_collapse"></b-nav-toggle>
+                <img src="/static/img/foodlovers.png" width="158px">
                 <b-button class="btn btn-outline-secondary"><i class="fa fa-cog" aria-hidden="true"></i></b-button>    
-                  <b-collapse is-nav id="nav_collapse">
+                <b-collapse is-nav id="nav_collapse">
                     <b-nav is-nav-bar>
                           <b-nav-item href="#">Perfil</b-nav-item>
                           <b-nav-item href="#">Preferências</b-nav-item>
                           <b-nav-item href="#">Combinações</b-nav-item>
                     </b-nav>
                 </b-collapse>
-        </nav> <br>
-   <!-- END Navbar -->
+        </nav><br>
+       <!-- END Navbar -->
    
-   <!-- Nav -->
-    <div class="nav nav-pills nav-fill">
-        <div class="nav-item">
-            <a class="nav-link" href="#"><i class="fa fa-camera" aria-hidden="true"></i></a>
-        </div><div class="nav-item">
-            <a class="nav-link active" href="#"><i class="fa fa-heart" aria-hidden="true"></i></a>
-        </div>
-        <div class="nav-item">
-            <a class="nav-link" href="#"><i class="fa fa-comments" aria-hidden="true"></i></a>
-        </div>
-    </div> <br>    
-    <!--END Nav -->
+       <!-- Nav -->
+        <div class="nav nav-pills nav-fill">
+            <div class="nav-item">
+            <router-link class="nav-link" role="a" aria-pressed="true" to="/usuario-nao-autenticado"><i class="fa fa-camera" aria-hidden="true"></i></router-link>
+            </div>
+            <div class="nav-item">
+            <router-link class="nav-link active" role="a" aria-pressed="true" to="/inicio"><i class="fa fa-heart" aria-hidden="true"></i></router-link>
+            </div>
+            <div class="nav-item">
+            <router-link class="nav-link" role="a" aria-pressed="true" to="/chat"><i class="fa fa-comments" aria-hidden="true"></i></router-link>
+            </div>
+        </div> <br>    
+        <!--END Nav -->
     
-    <!-- Perfil Card -->
-    <div class="row justify-content-center">
-        <div>
-           <b-card-group>
-                <b-card class="profile">
-                    <div slot="footer" class="d-flex justify-content-around">
-                        <b-button class="btn btn-circle btn-danger"><i class="fa fa-times" aria-hidden="true"></i></b-button>
-                        <b-button v-b-toggle="'collapse2'" class="btn btn-circle btn-info m-1"><i class="fa fa-info-circle" aria-hidden="true"></i></b-button>
-                        <b-button class="btn btn-circle btn-success"><i class="fa fa-check" aria-hidden="true"></i></b-button> 
-                    </div>
-                    <b-collapse id="collapse2">
-                        <h1>Kali Meinar, 23</h1>
-                        <p><i class="fa fa-map-marker" aria-hidden="true"></i>  São Paulo, SP - Brasil</p>
-                        <h6>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </h6>
-                        <h2>
-                        <img src="/static/img/batataico.png" alt="icoFrituras" width="50px" height="50px">
-                        <img src="/static/img/boloico.png" alt="icoDoces" width="50px" height="50px">
-                        <img src="/static/img/cafeico.png" alt="icoBebidasHot" width="50px" height="50px">
-                        <img src="/static/img/icosand.png" alt="icoLanches" width="50px" height="50px">
-                        <img src="/static/img/pizzaico.png" alt="icoPizza" width="50px" height="50px">
-                        </h2>
-                  </b-collapse>
-                </b-card>
-            </b-card-group>
+        <!-- Perfil Card -->
+        <div class="row justify-content-center">
+            <div>
+               <b-card-group>
+                    <b-card class="profile">
+                        <div slot="footer" class="d-flex justify-content-around">
+                            <b-button class="btn btn-circle btn-danger"><i class="fa fa-times" aria-hidden="true"></i></b-button>
+                            <b-button v-b-toggle="'collapse2'" class="btn btn-circle btn-info m-1"><i class="fa fa-info-circle" aria-hidden="true"></i></b-button>
+                            <b-button class="btn btn-circle btn-success"><i class="fa fa-check" aria-hidden="true"></i></b-button> 
+                        </div>
+                        <b-collapse id="collapse2">
+                            <h1>Kali Meinar, 23</h1>
+                            <p><i class="fa fa-map-marker" aria-hidden="true"></i>  São Paulo, SP - Brasil</p>
+                            <h6>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </h6>
+                            <p>
+                            <img src="/static/img/batataico.png" alt="icoFrituras" width="50px" height="50px">
+                            <img src="/static/img/boloico.png" alt="icoDoces" width="50px" height="50px">
+                            <img src="/static/img/cafeico.png" alt="icoBebidasHot" width="50px" height="50px">
+                            <img src="/static/img/icosand.png" alt="icoLanches" width="50px" height="50px">
+                            <img src="/static/img/pizzaico.png" alt="icoPizza" width="50px" height="50px">
+                            </p>
+                      </b-collapse>
+                    </b-card>
+                </b-card-group>
+            </div>
         </div>
-    </div>
-    <!-- END Perfil Card -->
+        <!-- END Perfil Card -->
 
        </div>
   </section>
@@ -104,9 +105,9 @@
         background-color: #ecf0f1;
         border-style: hidden;
         box-sizing: border-box;
-        border-radius: 10px;
+        border-radius: 15px;
         overflow: hidden;
-        box-shadow: 0 20px 20px rgba(0,0,0,.2);
+        box-shadow: 10px 20px 20px rgba(0,0,0,.5);
         background-image: url('/static/img/m5.jpg');
         background-size: cover;
         align-content: stretch;
@@ -120,21 +121,13 @@
         border-radius: 25px;
         
     }
-   
-    .container b-card {
-       height: 100%;
-       width: 100%;
-       background-image: url(/static/img/m5.jpg);
-    }
     
     #collapse2 {
         background-color: #f7f7f7;
-        box-sizing: border-box;
-        border-radius: 20px;
+        border-radius: 15px;
         overflow: hidden;
-        box-shadow: 0 30px 20px rgba(0,0,0,.2);
+        box-shadow: 0 30px 30px rgba(0,0,0,.6);
         text-align: center;
-        padding: 13px;
     }
     #collapse2 h1 {
         font-family: 'Raleway:400', sans-serif;
@@ -150,13 +143,6 @@
         color: #3498db;
         border-bottom: 0.2px solid;
         border-bottom-color: #ecf0f1;
-        border-top: 0.2px solid;
-        border-top-color: #ecf0f1;
-        text-align: justify;
-        padding: 7px;
-    }
-    #collapse2 h2 {
-        color: #3498db;
         border-top: 0.2px solid;
         border-top-color: #ecf0f1;
         text-align: center;
