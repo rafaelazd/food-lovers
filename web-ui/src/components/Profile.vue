bb748629a9c7af2e38aaa01c336bd2d6674ba783
Rafaela Zanetti <rafazanettid@gmail.com>
2017-12-06 00:01:27 -0200

    Adicionando a barra de preferências do usuário nos Cards
    Adicionado a barra de preferências do usuário nos Cards da página início

--- web-ui/src/components/Profile.vue
+++ web-ui/src/components/Profile.vue
@@ -7,23 +7,28 @@
         </div>
         <b-collapse id="collapse2">
             <b-button v-b-toggle="'collapse2'" class="btn btn-circle btn-times btn-danger float-left m-1"><i class="fa fa-times" aria-hidden="true"></i></b-button><br>
-            <h1>{{profile.nome}} {{profile.sobrenome}}, {{profile.idade}}</h1>
-            <p><i class="fa fa-map-marker" aria-hidden="true"></i> {{profile.local}}</p>
-            <h6>{{profile.biografia}}</h6>
+                <h1>{{profile.nome}} {{profile.sobrenome}}, {{profile.idade}}</h1>
+                <p><i class="fa fa-map-marker" aria-hidden="true"></i> {{profile.local}}</p>
+                <h6>{{profile.biografia}}</h6>
             <p>
-            <img src="/static/img/batataico.png" alt="icoFrituras" width="40px" height="40px">
+            <!--<img src="/static/img/batataico.png" alt="icoFrituras" width="40px" height="40px">
             <img src="/static/img/cafeico.png" alt="icoBebidasHot" width="40px" height="40px">
-            <img src="/static/img/pizzaico.png" alt="icoPizza" width="40px" height="40px">
+            <img src="/static/img/pizzaico.png" alt="icoPizza" width="40px" height="40px">-->
+                <b-row class="d-flex justify-content-center" id="minicons">
+                    <b-col  v-for="pref in prefs"  :key="pref.id" class="prefs-icon" cols="2">
+                        <img :src="pref.filepath" alt="">
+                    </b-col>
+                </b-row>
             </p>
             <p>
-            <b-button class="btn btn-sm btn-secondary" :href="toPerfil"><i class="fa fa-id-card-o" aria-hidden="true"></i> Ver Perfil</b-button>
+                <b-button class="btn btn-sm btn-secondary" :href="toPerfil"><i class="fa fa-id-card-o" aria-hidden="true"></i> Ver Perfil</b-button>
             </p>
         </b-collapse>
     </b-card>
 </template>
 
 <script>
-
+import axios from 'axios'
  export default { 
      name: 'Profile', 
 
@@ -36,6 +41,7 @@
      
      created() {
          console.log(this.profile.idusu);
+         this.buscarPref();
      },
      
      data() {
@@ -43,7 +49,8 @@
              image: "/static/img/m5.jpg",
              toPerfil: '#/perfil',
              clckNo: false,
-             clckYes: false
+             clckYes: false,
+             prefs: []
          }
      },
      
@@ -53,12 +60,33 @@
          },
          funcYes () {
              this.clckYes = true
-         }
+         }, 
+         
+         buscarPref() {
+                axios.get('http://localhost:8060/pessoas/'+ this.profile.idusu +'/preferencias')
+                    .then((resp) => {
+                        this.erro = false;
+                        this.prefs = resp.data._embedded.preferencias;
+                        console.log(resp.data);
+                    })
+                    .catch((err) => {
+                        this.erro = true;
+                        console.log(err)
+                    });
+            }
      }
 };
 </script>
 
 <style>
+    .prefs-icon img{
+        width: 50px;
+        height: 50px;
+    }
+    
+    #minicons {
+       margin-left: -45px; 
+    }
     
     .swipe-left {
         animation: swipe-left 1s forwards;
